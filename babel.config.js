06f4288d7bffccb90eb91390fe22f4d013b66330
auliakbarh <auliaakbarharahaap@gmail.com>
2023-05-14 04:21:02 +0700

    :wrench: configure typescript

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
+const presets = ['module:metro-react-native-babel-preset'];
+
+const plugins = [];
+
+plugins.push([
+  'module-resolver',
+  {
+    root: ['./src'],
+    extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+    alias: {
+      '@': './src',
+    },
+  },
+]);
+
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets,
+  plugins,
 };
